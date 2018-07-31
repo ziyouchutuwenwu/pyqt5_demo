5ffb0c99e7a217337ae68232c826169fedcdc3e0
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2018-07-31 19:34:56 +0800

    添加webkit

--- main_window.py
+++ main_window.py
@@ -1,4 +1,6 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QUrl
+
 from ui_main_window import Ui_MainWindow
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -10,3 +12,4 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def my_button_clicked(self):
         self._infoLabel.setText("Hello World")
+        self._webView.setUrl(QUrl('http://www.baidu.com'))
--- ui_main_window.py
+++ ui_main_window.py
@@ -2,25 +2,34 @@
 
 # Form implementation generated from reading ui file 'ui_main_window.ui'
 #
-# Created by: PyQt5 UI code generator 5.10
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
-
+from PyQt5 import QtWebEngineWidgets
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(581, 283)
+        MainWindow.resize(298, 228)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.myButton = QtWidgets.QPushButton(self.centralwidget)
-        self.myButton.setGeometry(QtCore.QRect(240, 190, 75, 23))
-        self.myButton.setObjectName("myButton")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self._webView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+        self._webView.setUrl(QtCore.QUrl("about:blank"))
+        self._webView.setObjectName("_webView")
+        self.verticalLayout.addWidget(self._webView)
         self._infoLabel = QtWidgets.QLabel(self.centralwidget)
-        self._infoLabel.setGeometry(QtCore.QRect(190, 90, 161, 71))
         self._infoLabel.setAlignment(QtCore.Qt.AlignCenter)
         self._infoLabel.setObjectName("_infoLabel")
+        self.verticalLayout.addWidget(self._infoLabel)
+        self.myButton = QtWidgets.QPushButton(self.centralwidget)
+        self.myButton.setObjectName("myButton")
+        self.verticalLayout.addWidget(self.myButton)
+        self.horizontalLayout.addLayout(self.verticalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -29,6 +38,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.myButton.setText(_translate("MainWindow", "PushButton"))
         self._infoLabel.setText(_translate("MainWindow", "TextLabel"))
-
+        self.myButton.setText(_translate("MainWindow", "PushButton"))
